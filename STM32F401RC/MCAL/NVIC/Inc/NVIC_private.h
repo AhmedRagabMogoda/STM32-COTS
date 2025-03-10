310b5099bc82bb95b581dd504d85c94de551c369
Ahmed Mogoda <ahmedragabmogouda@gmail.com>
2025-03-11 00:33:03 +0200

    NVIC Driver

---
+++ STM32F401RC/MCAL/NVIC/Inc/NVIC_private.h
@@ -0,0 +1,22 @@
+/*
+ *  NVIC_private.h
+ *
+ *  Created on: Nov 30, 2024
+ *  Author: Ahmed Ragab
+ */
+
+#ifndef NVIC_PRIVATE_H_
+#define NVIC_PRIVATE_H_
+
+#define PIN_SHIFTING             32u
+#define ISER_MASK                1u
+#define ICER_MASK                1u
+#define ISPR_MASK                1u
+#define ICPR_MASK                1u
+#define IABR_MASK                1u
+
+#define IPR_MASK                 0b1111
+#define IPR_PIN_ACCESS           4u
+
+
+#endif /* NVIC_PRIVATE_H_ */
